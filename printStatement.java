057d906c24058969a236548181a347daa7f5e0b6
theGreatestIntelligent <theGreatestIntelligent@gmail.com>
2022-03-13 21:46:14 +0530

    just the missing things are added

---
+++ printStatement.java
@@ -0,0 +1,14 @@
+package learnJava;
+
+public class learnJava { //represents a opening of class
+
+	public static void main(String[] args) {
+		
+		System.out.print("Hi , theGreatesIntelligent Mr.Gowtham.S"); //This is a print statement,S in System must be capital
+		
+		System.out.println("Hi , theGreatesIntelligent Mr.Gowtham.S"); // println refers to new line of text
+		
+	}
+
+}
+
---
+++ variables.java
@@ -0,0 +1,17 @@
+package learnJava;
+
+public class learnJava { //represents a opening of class
+
+	public static void main(String[] args) {
+		
+		String x = "hi"; //stores series of words , S must be capital
+		int y = 2; //stores full values
+		float z = 3.6F; //stores decimal with 7 precission,always ends with capital F
+		double a = 3.6; //store decimal upto 15
+		boolean b = true; //stores the value of either true or false
+		char c = 'a'; //stores a ssingle letter,always in single quote
+
+	}
+
+}
+
