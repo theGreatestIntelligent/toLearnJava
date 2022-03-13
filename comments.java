75246905345aa93669323b67191ba4260101c7b2
theGreatestIntelligent <theGreatestIntelligent@gmail.com>
2022-03-13 21:29:58 +0530

    comments notes

---
+++ comments.java
@@ -0,0 +1,15 @@
+public package learnJava;
+
+public class learnJava {
+
+	public static void main(String[] args) {
+		
+		//This is a comment (Slases Are Indicate For Single Line Comment)
+
+        /* Used
+        to
+        indicate
+        multiline
+        comment */
+
+	}
